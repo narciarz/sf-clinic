2d9321550d131dcdec15f936fc8f9b12e596ea0c
MarcinW <wiewmarcin@gmail.com>
2018-08-10 23:06:19 +0200

    Initial Project

---
+++ src/main/java/com/mawi/sfclinic/SfClinicApplication.java
@@ -0,0 +1,12 @@
+package com.mawi.sfclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfClinicApplication.class, args);
+    }
+}
