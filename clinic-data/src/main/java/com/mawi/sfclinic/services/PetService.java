89e9ec07f3b4f33fd006c8118e216f719e7921f3
MarcinW <wiewmarcin@gmail.com>
2018-09-28 22:20:43 +0200

    Created interfaces for services

---
+++ clinic-data/src/main/java/com/mawi/sfclinic/services/PetService.java
@@ -0,0 +1,16 @@
+package com.mawi.sfclinic.services;
+
+
+import com.mawi.sfclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
---
+++ clinic-data/src/main/java/com/mawi/sfclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.mawi.sfclinic.services;
+
+import com.mawi.sfclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
