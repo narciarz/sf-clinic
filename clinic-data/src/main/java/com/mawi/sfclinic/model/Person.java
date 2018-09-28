6ce64e1decb966557a1805cebfa42c0c4f4b7e2c
MarcinW <wiewmarcin@gmail.com>
2018-09-28 22:27:21 +0200

    Created base entity

--- clinic-data/src/main/java/com/mawi/sfclinic/model/Person.java
+++ clinic-data/src/main/java/com/mawi/sfclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.mawi.sfclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- clinic-data/src/main/java/com/mawi/sfclinic/model/PetType.java
+++ clinic-data/src/main/java/com/mawi/sfclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.mawi.sfclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
