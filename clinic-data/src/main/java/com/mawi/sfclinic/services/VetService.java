293aa60741d86dd4b889b91c8edf4bdeae82cf58
MarcinW <wiewmarcin@gmail.com>
2018-10-02 22:30:48 +0200

    Refactoring allowing using common Crud interface. closes #1

--- clinic-data/src/main/java/com/mawi/sfclinic/services/VetService.java
+++ clinic-data/src/main/java/com/mawi/sfclinic/services/VetService.java
@@ -2,14 +2,6 @@ package com.mawi.sfclinic.services;
 
 import com.mawi.sfclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
