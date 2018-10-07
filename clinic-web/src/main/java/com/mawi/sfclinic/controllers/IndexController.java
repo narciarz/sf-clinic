40ad3ad33c632c6a1153e74c47217c9ba8c9d094
MarcinW <wiewmarcin@gmail.com>
2018-10-07 18:44:09 +0200

    Added Index Page + controller. Closes #4

---
+++ clinic-web/src/main/java/com/mawi/sfclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.mawi.sfclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+
+}
