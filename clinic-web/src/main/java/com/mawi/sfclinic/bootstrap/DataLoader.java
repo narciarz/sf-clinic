51ef7b247d0c24285ec0edd15311cd102d9fb869
MarcinW <wiewmarcin@gmail.com>
2018-11-04 09:47:43 +0100

    Added PetTypes to Bootstrap. Closes #26

--- clinic-web/src/main/java/com/mawi/sfclinic/bootstrap/DataLoader.java
+++ clinic-web/src/main/java/com/mawi/sfclinic/bootstrap/DataLoader.java
@@ -2,10 +2,11 @@ package com.mawi.sfclinic.bootstrap;
 
 
 import com.mawi.sfclinic.model.Owner;
+import com.mawi.sfclinic.model.PetType;
 import com.mawi.sfclinic.model.Vet;
 import com.mawi.sfclinic.services.OwnerService;
+import com.mawi.sfclinic.services.PetTypeService;
 import com.mawi.sfclinic.services.VetService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,16 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded PetTypes ....");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
